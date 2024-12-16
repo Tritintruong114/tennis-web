0f4fb84b1146ed8fc3fc553cd8558c67231313e7
brunotruong <113487900+Tritintruong114@users.noreply.github.com>
2024-12-16 21:08:33 +0700

    feat: update header

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -1,22 +1,57 @@
+"use client";
 import React from "react";
 import { header } from "@/data/header.json";
+
+import cx from "classnames";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useWindowSize } from "usehooks-ts";
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
+
+  const size = useWindowSize();
+
+  useEffect(() => {
+    if (size.width > 768) {
+      setShowMenu(false);
+    }
+  }, [size.width]);
+
   return (
-    <nav className="w-screen flex items-center justify-center py-6 border-b">
-      <ul className="flex w-full justify-between max-w-3xl items-center">
+    <nav className="flex w-screen items-center justify-between sm:justify-center p-6 border-b">
+      <ul
+        className={cx(
+          "flex top-0 right-0  bg-white flex-col sm:translate-y-0 sm:flex-row duration-150 w-full justify-center sm:justify-between max-w-3xl items-center absolute sm:relative h-screen sm:h-full",
+          showMenu ? "translate-y-visible-menu" : "translate-y-hidden-menu"
+        )}
+      >
         {header.menuItems.map((item, index) => (
           <li
             key={index}
-            className="flex items-center group justify-center tracking-tighter relative"
+            className="flex items-center group py-6 justify-center tracking-tighter relative text-sub-title"
           >
             <Link className="font-coolvetica" href={item.url}>
               {item.label}
             </Link>
-            <div className="bg-[#ceff65] h-[.1rem] left-0 absolute group-hover:w-full w-0 duration-300 p-0"></div>
+            <div className="bg-highlight-green h-[.1rem] left-0 absolute group-hover:w-full w-0 duration-300 p-0"></div>
           </li>
         ))}
       </ul>
+      <div className="relative sm:hidden font-coolvetica uppercase text-sub-title cursor-pointer">
+        <Link href="/">Tennis Academy</Link>
+      </div>
+      <span
+        className="material-symbols-outlined sm:hidden relative cursor-pointer"
+        onClick={toggleMenu}
+      >
+        {showMenu ? "close" : "menu"}
+      </span>
     </nav>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,58 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        "highlight-green": "#ceff65",
+        "bg-white": "#ffffff",
+      },
+      container: {
+        // Container System
+        center: true, // Center containers by default
+        padding: {
+          DEFAULT: "1rem", // Default container padding
+          sm: "2rem", // Small screens
+          lg: "3rem", // Large screens
+          xl: "4rem", // Extra large screens
+        },
+        screens: {
+          sm: "640px", // Small screen width
+          md: "768px", // Medium screen width
+          lg: "1024px", // Large screen width
+          xl: "1280px", // Extra large screen width
+          "2xl": "1536px", // 2x large screen width
+        },
+      },
+      borderRadius: {
+        sm: "0.25rem",
+        md: "0.5rem",
+        lg: "1rem",
+        xl: "2rem",
+      },
+      maxWidth: {
+        container: "1200px", // Maximum container width
+        "content-sm": "640px", // Small content width
+        "content-lg": "960px", // Large content width
+        "content-xl": "1200px", // Extra large content width
+      },
+
       fontFamily: {
         satoshi: ["Satoshi", "sans"],
         coolvetica: ["Coolvetica", "sans-serif"],
+      },
+      fontSize: {
+        sm: "clamp(0.8rem, 0.17vw + 0.76rem, 0.89rem)",
+        base: "clamp(1rem, 0.34vw + 0.91rem, 1.19rem)",
+        lg: "clamp(1.25rem, 0.61vw + 1.1rem, 1.58rem)",
+        label: "clamp(1rem, 1vw + 1.31rem, 1.2rem)",
+        "sub-title": "clamp(1rem, 1.56vw + 1.56rem, 1.25rem)",
+        title: "clamp(2.44rem, 2.38vw + 1.85rem, 2.8rem)",
+        features: "clamp(3.05rem, 3.54vw + 2.17rem, 5rem)",
+        section: "clamp(3.81rem, 5.18vw + 2.52rem, 4rem)",
+        hero: "clamp(4.77rem, 7.48vw + 2.9rem, 6rem)",
+      },
+      translate: {
+        "hidden-menu": "-100%", // For hiding the menu
+        "visible-menu": "0%", // For showing the menu
       },
     },
   },
